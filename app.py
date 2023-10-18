c4756ea19f2285b66edb783f9a9d02d4c3aabcab
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-19 02:29:27 +0800

    add error handling in create_item and implement abort

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -22,8 +23,26 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -42,7 +61,7 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -50,4 +69,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
