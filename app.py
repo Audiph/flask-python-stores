c849050b2ab1b02b02d9fbdb0755c037dd7275f0
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-19 01:19:39 +0800

    add get_store and get_items from a store

--- app.py
+++ app.py
@@ -28,3 +28,21 @@ def create_item(name):
             return new_item, 201
 
     return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"store": store}
+
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/items")
+def get_items(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+
+    return {"message": "Store not found"}, 404
