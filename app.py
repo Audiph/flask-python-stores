4383f98688b54380d66e992a046abeff46f9cb99
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-19 00:54:59 +0800

    add create_store

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,3 +8,11 @@ stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
